0718cb712bce41d987cd18a197b3b2f40427d2b8
noyamirai <planet1109@hotmail.com>
2022-03-02 16:02:48 +0100

    added: dark mode and responsive animation

--- opdracht1/scripts/script.js
+++ opdracht1/scripts/script.js
@@ -1,2 +1,22 @@
-// JavaScript Document
-console.log("howdy");
+console.log("TOGGLE SCRIPT: ACTIVE");
+
+const toggleContainer = document.querySelector("fieldset");
+const toggleBtn = document.querySelector("#color-mode");
+const bodyElement = document.querySelector("body");
+
+toggleContainer.classList.toggle("hide");
+
+if (localStorage.getItem('colorMode') == 'light') {
+    bodyElement.classList.add("lightmode"); 
+    toggleBtn.checked = true;
+}
+
+toggleBtn.addEventListener(`click`, event => {
+    if (!localStorage.getItem('colorMode')) {
+        localStorage.setItem('colorMode', 'light');
+    } else {
+        localStorage.clear();
+    }
+
+    bodyElement.classList.toggle("lightmode");
+});
