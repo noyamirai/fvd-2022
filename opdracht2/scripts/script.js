6c091baaec751bfcd218bfcf353c8c293593c4bf
noyamirai <planet1109@hotmail.com>
2022-03-23 17:59:22 +0100

    feat(library): added styling for library

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,2 +1,25 @@
-// JavaScript Document
-console.log("howdy");
+console.log("FILTER BTN REVEAL SCRIPT: ACTIVE");
+
+const bodyEl = document.querySelector("body");
+const checkBoxes = document.querySelectorAll(".filters__option");
+
+const btnContainer = document.querySelector(".btn-container");
+const dismissBtn = document.querySelector("#btn-dismiss");
+
+checkBoxes.forEach(box => {
+    box.addEventListener("change", () => {
+        bodyEl.classList.toggle("overlay");
+        btnContainer.classList.toggle("visible");
+    });
+});
+
+dismissBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    checkBoxes.forEach(box => {
+        box.checked = false;
+    })
+    
+    bodyEl.classList.toggle("overlay");
+    btnContainer.classList.toggle("visible");
+})
