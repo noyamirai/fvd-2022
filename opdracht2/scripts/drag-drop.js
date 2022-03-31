bc747aa37e17a400b168887b6f5ee2d0349151ab
noyamirai <planet1109@hotmail.com>
2022-03-31 13:02:12 +0200

    feat(localstorage): saved/remove

--- opdracht2/scripts/drag-drop.js
+++ opdracht2/scripts/drag-drop.js
@@ -17,51 +17,141 @@ const faveGameCollection = document.querySelector('#fave-game-collection');
 const completedGameCollection = document.querySelector('#completed-game-collection');
 
 let counter = 0;
+let gameClone;
 
 // TODO (GLOBAL): REFACTOR AND PUT IN FUNCTIONS
 
-window.addEventListener('load', (event) => {
-    // const savedFaves = JSON.parse(localStorage.getItem("saved-faves") || "[]");
+window.addEventListener('load', () => {
+    const savedFaves = JSON.parse(localStorage.getItem("saved-faves") || "[]");
+    const savedCompleted = JSON.parse(localStorage.getItem("saved-completed") || "[]");
+
+    // TODO: LIST__EMPTY
+
+    if (savedFaves.length && savedCompleted.length) {
+        faveGameCollection.classList.toggle('list__empty');
+        faveGameCollection.innerHTML = savedFaves;
+
+        completedGameCollection.classList.toggle('list__empty');
+        completedGameCollection.innerHTML = savedCompleted;
+        
+        faveGameCollection.querySelectorAll('li').forEach((item) => {
+            addListeners(item);
+        });
+
+        completedGameCollection.querySelectorAll('li').forEach((item) => {
+            addListeners(item);
+        });
+
+    } else if (savedFaves.length && !savedCompleted.length) {
+        faveGameCollection.classList.toggle('list__empty');
+        faveGameCollection.innerHTML = savedFaves;
+
+        faveGameCollection.querySelectorAll('li').forEach((item) => {
+            addListeners(item);
+        });
+
+    } else if (!savedFaves.length && savedCompleted.length) {
+        completedGameCollection.classList.toggle('list__empty');
+        completedGameCollection.innerHTML = savedCompleted;
+
+        completedGameCollection.querySelectorAll('li').forEach((item) => {
+            addListeners(item);
+        });
+    }
     
-    // TODO: CHECK IF ANYTHING IS SAVED AND IN WHICH COLLECTION
-    // faveGameCollection.classList.toggle('list__empty');
-    // faveGameCollection.innerHTML = savedFaves;
 });
 
+const saveData = (collection) => {
+    if (collection.id == 'fave-game-collection') {
+        console.log('added to faves');
+        localStorage.setItem('saved-faves', JSON.stringify(collection.innerHTML));
+
+    } else if (collection.id == 'completed-game-collection') {
+        console.log('added to completed');
+        localStorage.setItem('saved-completed', JSON.stringify(collection.innerHTML));
+    }
+};
+
+const getClone = (toBeClonedItem, fromAction) => {
+    let nodeCopy = toBeClonedItem.cloneNode(true);
+
+    nodeCopy.removeAttribute('class');
+    nodeCopy.querySelector('.add-game').classList.toggle('hide');
+    nodeCopy.querySelector('.remove-game').classList.toggle('hide');
+    nodeCopy.draggable = false;
+
+    if (fromAction == 'click') {
+        nodeCopy.id = `${toBeClonedItem.id}-copy`;
+
+    } else if (fromAction == 'drag') {
+        nodeCopy.id = `${toBeClonedItem.id}-copy`;
+    }
+
+    return nodeCopy;
+};
+
+const activateNotification = (notificationLabel, type) => {
+    notificationText.textContent = notificationLabel;
+
+    if (type == 'removed') {
+        notification.classList.remove('notification--added');
+        notification.classList.toggle('notification--open');
+        notification.classList.toggle('notification--removed');
+
+    } else if (type == 'added') {
+        notification.classList.remove('notification--removed');
+        notification.classList.toggle('notification--open');
+        notification.classList.toggle('notification--added');
+    }
+
+    setTimeout(() => {
+        notification.classList.remove('notification--open');
+    }, 2000);
+};
+
+const addToCollection = (gameCollection, clonedItem) => {
+    gameCollection.classList.remove('list__empty');
+    gameCollection.appendChild(clonedItem);
+    addListeners(clonedItem);
+
+    toggleActionContainer();
+};
 
 const addListeners = (cloneEl) => {
     const cloneButton = cloneEl.querySelector('.remove-game');
 
+    // REMOVE BUTTON
     cloneButton.addEventListener('click', (e) => {
+        const collection = e.target.closest('ol');
 
-        if (e.target.closest('ol').id == 'fave-game-collection') {
-            const collection = faveGameCollection;
+        if (collection.id == 'fave-game-collection') {
             const allItems = collection.getElementsByTagName('li');
-            
-            e.target.closest('li').remove();                    
+
+            e.target.closest('li').remove();
+            saveData(collection);
+
             if (!allItems.length) {
                 collection.classList.add('list__empty');
             }
-        } else if (e.target.closest('ol').id == 'completed-game-collection') {
-            const collection = completedGameCollection;
+        } else if (collection.id == 'completed-game-collection') {
             const allItems = collection.getElementsByTagName('li');
-            
-            e.target.closest('li').remove();                    
+
+            e.target.closest('li').remove();
+            saveData(collection);
+
             if (!allItems.length) {
                 collection.classList.add('list__empty');
             }
         }
 
-        notificationText.textContent = 'Game has been removed from list!'
-        notification.classList.remove('notification--added');        
-        notification.classList.toggle('notification--open');
-        notification.classList.toggle('notification--removed');
-
-        setTimeout(() => {
-            notification.classList.remove('notification--open');
-        }, 1500);
+        activateNotification('Game has been removed from list!', 'removed');
     });
-}
+};
+
+const toggleActionContainer = () => {
+    bodyEl.classList.toggle("overlay");
+    gameActionContainer.classList.toggle('visible');
+};
 
 const allowDrop = (e) => {
     e.preventDefault();
@@ -71,35 +161,21 @@ const onDrop = (e) => {
     e.preventDefault();
 
     const data = e.dataTransfer.getData("text");
-
-    let nodeCopy = document.getElementById(data).cloneNode(true);
-    nodeCopy.id = `${data}-copy`;
-    nodeCopy.removeAttribute('class');
-    nodeCopy.querySelector('.add-game').classList.toggle('hide');
-    nodeCopy.querySelector('.remove-game').classList.toggle('hide');
-    nodeCopy.draggable = false;
+    const clonedItem = getClone(document.getElementById(data), 'drag');
 
     if (e.target.tagName.toLowerCase() == 'li') {
         const newTarget = e.target.closest('ol');
-        newTarget.appendChild(nodeCopy);
-        addListeners(nodeCopy);
+        newTarget.appendChild(clonedItem);
+        addListeners(clonedItem);
 
     } else {
-        e.target.appendChild(nodeCopy);
-        addListeners(nodeCopy);
+        e.target.appendChild(clonedItem);
+        addListeners(clonedItem);
     }
 
-    notificationText.textContent = 'Game has been added to list!'
-    notification.classList.remove('notification--removed');        
-    notification.classList.toggle('notification--open');
-    notification.classList.toggle('notification--added');
+    activateNotification('Game has been added to list!', 'added');
 
-    setTimeout(() => {
-        notification.classList.remove('notification--open');
-    }, 1500);
-
-    // TODO: CHECK IN WHICH COLLECTION IT WAS DROPPED
-    // localStorage.setItem('saved-faves', JSON.stringify(e.target.innerHTML));
+    saveData(e.target);
 
     e.target.classList.remove('list__empty');
 };
@@ -109,119 +185,96 @@ const drag = (e) => {
 };
 
 dragContainer.forEach((item) => {
-    item.addEventListener('dragover', (e) => {
-        if (!e.target.classList.contains('list__drag-container--block')) {
-            allowDrop(e);
-        }
-    });
+    if (!(window.innerWidth <= 950)) {
 
-    item.addEventListener('drop', (e) => {
-        onDrop(e);
-    })
+        item.addEventListener('dragover', (e) => {
+            if (!e.target.classList.contains('list__drag-container--block')) {
+                allowDrop(e);
+            }
+        });
+
+        item.addEventListener('drop', (e) => {
+            onDrop(e);
+        });
+    }
 });
 
 draggableItems.forEach((listItem) => {
-    listItem.draggable = true;
-    listItem.id = `drag-${counter++}`;
 
-    listItem.addEventListener('dragstart', (e) => {
-        dragContainer.forEach((container) => {
+    // IF DESKTOP SIZE
+    if (!(window.innerWidth <= 950)) {
 
-            const allItems = container.getElementsByTagName('li');
+        listItem.classList.remove('draggable--mobile');
+        listItem.draggable = true;
+        listItem.id = `drag-${counter++}`;
 
-            // Check if container already has games
-            if (allItems.length) {
-                for (let game of allItems) {
-                    // If game is already inside list
-                    if (game.id == `${listItem.id}-copy`) {
-                        container.classList.add('list__drag-container--block');
-                    } else {
-                        container.classList.add('list__empty--active');
+        listItem.addEventListener('dragstart', (e) => {
+            dragContainer.forEach((container) => {
+
+                const allItems = container.getElementsByTagName('li');
+
+                // Check if container already has games
+                if (allItems.length) {
+                    for (let game of allItems) {
+                        // If game is already inside list
+                        if (game.id == `${listItem.id}-copy`) {
+                            container.classList.add('list__drag-container--block');
+                        } else {
+                            container.classList.add('list__empty--active');
+                        }
                     }
+                } else {
+                    container.classList.add('list__empty--active');
                 }
-            } else {
-                container.classList.add('list__empty--active');
-            }
+            });
+            drag(e);
         });
-        drag(e);
-    });
 
-    listItem.addEventListener('dragend', (e) => {
-        dragContainer.forEach((item) => {
-            item.classList.remove('list__empty--active');
-            item.classList.remove('list__drag-container--block');
+        listItem.addEventListener('dragend', (e) => {
+            dragContainer.forEach((item) => {
+                item.classList.remove('list__empty--active');
+                item.classList.remove('list__drag-container--block');
+            });
         });
-    });
+    } else {
+        listItem.classList.add('draggable--mobile');
+
+        listItem.querySelector('picture').addEventListener('click', (e) => {
+            e.preventDefault();
+
+            gameClone = getClone(e.target.closest('li'), 'click');
+            toggleActionContainer();
+        });
+    }
 });
-
-let gameClone;
 
 addGameButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
+        gameClone = getClone(e.target.closest('li'), 'click');
 
-        let nodeCopy = e.target.closest('li').cloneNode(true);
-        nodeCopy.id = `${e.target.closest('li').id}-copy`;
-        nodeCopy.removeAttribute('class');
-        nodeCopy.querySelector('.add-game').classList.toggle('hide');
-        nodeCopy.querySelector('.remove-game').classList.toggle('hide');
-        nodeCopy.draggable = false;
-
-        gameClone = nodeCopy;
-        // TODO: SAVE IN LOCAL STORAGE
-
-        bodyEl.classList.toggle("overlay");
-        gameActionContainer.classList.toggle('visible');
-
+        toggleActionContainer();
     });
 });
 
 addToFaves.addEventListener('click', (e) => {
     const gameItem = gameClone;
 
-    faveGameCollection.classList.remove('list__empty');
-    faveGameCollection.appendChild(gameItem);
-    addListeners(gameItem);
+    addToCollection(faveGameCollection, gameItem);
 
-    bodyEl.classList.toggle("overlay");
-    gameActionContainer.classList.toggle('visible');
-
-    // TODO: SAVE IN LOCAL STORAGE
-    // TODO: ADD DELAY HERE?
-    notificationText.textContent = 'Game has been added to list!'
-    notification.classList.remove('notification--removed');        
-    notification.classList.toggle('notification--open');
-    notification.classList.toggle('notification--added');
-
-    setTimeout(() => {
-        notification.classList.remove('notification--open');
-    }, 1500);
+    activateNotification('Game has been added to list!', 'added');
+    saveData(collection);
 });
 
 addToCompleted.addEventListener('click', (e) => {
     const gameItem = gameClone;
 
-    completedGameCollection.classList.remove('list__empty');
-    completedGameCollection.appendChild(gameItem);
-    addListeners(gameItem);
+    addToCollection(completedGameCollection, gameItem);
 
-    bodyEl.classList.toggle("overlay");
-    gameActionContainer.classList.toggle('visible');
-    
-    // TODO: SAVE IN LOCAL STORAGE
-    // TODO: ADD DELAY HERE?
-    notificationText.textContent = 'Game has been added to list!'
-    notification.classList.remove('notification--removed');        
-    notification.classList.toggle('notification--open');
-    notification.classList.toggle('notification--added');
-
-    setTimeout(() => {
-        notification.classList.remove('notification--open');
-    }, 1500);
+    activateNotification('Game has been added to list!', 'added');
+    saveData(collection);
 });
 
 dismissBtn.addEventListener("click", (e) => {
     e.preventDefault();
-
-    bodyEl.classList.toggle("overlay");
-    gameActionContainer.classList.toggle("visible");
+    toggleActionContainer();
 });
